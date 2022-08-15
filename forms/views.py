120238e1cebde63357cddb0eedd579a88735a803
Faree320 <100018647+Faree320@users.noreply.github.com>
2022-08-15 16:52:18 +0500

    first commit

--- forms/views.py
+++ forms/views.py
@@ -13,19 +13,21 @@ import os
 def upload_link_form(request):
     if request.method == "POST":
         form = GetLinkForm(request.POST)
-        urllib.request.urlretrieve(request.POST['link1'], f"./static/video/your_video_name1.mp4")
-        urllib.request.urlretrieve(request.POST['link2'], f"./static/video/your_video_name2.mp4")
-        urllib.request.urlretrieve(request.POST['link3'], f"./static/video/your_video_name3.mp4")
-        clip1 = VideoFileClip("./static/video/your_video_name1.mp4", audio=True).subclip(0, 5)
-        clip2 = VideoFileClip("./static/video/your_video_name2.mp4", audio=True).subclip(0, 5)
-        clip3 = VideoFileClip("./static/video/your_video_name3.mp4", audio=True).subclip(0, 5)
+        PATH_1 = "./static/video/your_video_name1.mp4"
+        PATH_2 = "./static/video/your_video_name2.mp4"
+        PATH_3 = "./static/video/your_video_name3.mp4"
+        urllib.request.urlretrieve(request.POST['link1'], PATH_1)
+        urllib.request.urlretrieve(request.POST['link2'], PATH_2)
+        urllib.request.urlretrieve(request.POST['link3'], PATH_3)
+        clip1 = VideoFileClip("./static/video/your_video_name1.mp4", audio=True)
+        clip2 = VideoFileClip("./static/video/your_video_name2.mp4", audio=True)
+        clip3 = VideoFileClip("./static/video/your_video_name3.mp4", audio=True)
         ran = random.randint(0, 1000)
         final_clip = clips_array([[clip1, clip2, clip3]])
         final_clip.write_videofile(f"./static/video/{ran}merged.mp4")
-        # time.sleep(3)
-        # os.remove("./static/video/your_video_name1.mp4")
-        # os.remove("./static/video/your_video_name2.mp4")
-        # os.remove("./static/video/your_video_name3.mp4")
+        os.remove(PATH_1)
+        os.remove(PATH_2)
+        os.remove(PATH_3)
     else:
         form = GetLinkForm()
 
--- video_merdge/settings.py
+++ video_merdge/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/video/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/video')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
