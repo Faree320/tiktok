129d79cc8a5b291ae9b21a94204c98f8934903d4
Faree320 <100018647+Faree320@users.noreply.github.com>
2022-08-15 13:02:08 +0500

    first commi

--- video_merdge/settings.py
+++ video_merdge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0z5xzvvq2nu&*^8izq2s79i1g!o^y38jjpjsug9nq$1qlvlxh8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
